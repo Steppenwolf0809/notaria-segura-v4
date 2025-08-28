dd99bf9daf0430e1ba2e517d50d2e08d4c464682
Jose Luis <joseluiszapataec@gmail.com>
2025-08-27 23:51:45 -0500

    nuevo motor de concuerdos

---
+++ backend/src/services/extractos/index.js
@@ -0,0 +1,7 @@
+// Índice público para motor y módulos de extractos inteligentes
+
+import ExtractoTemplateEngine from './engine.js'
+import PoderTemplates from './poder.js'
+
+export { ExtractoTemplateEngine, PoderTemplates }
+
